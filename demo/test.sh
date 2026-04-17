e0eedfe9cfaa3e66af4abc894c25fee59b686afd
SeeleVolle <jjhuang5@outlook.com>
2026-04-17 13:49:36 +0000

    fix transfer version

--- demo/test.sh
+++
@@ -1,6 +0,0 @@
-for img in $(docker images -q); do
-    parent=$(docker inspect --format='{{.Parent}}' $img 2>/dev/null)
-    if [ "$parent" = "aacd11aa4d7f" ]; then
-        echo "Child image: $img"
-    fi
-done
